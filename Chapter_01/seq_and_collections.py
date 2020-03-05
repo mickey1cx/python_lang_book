ad4e55d9be0ffdffb59cd3f55e9b226d92deef2a
mickey1cx <mickey1cx@gmail.com>
2020-03-05 22:00:08 +0300

    ch 01, seq

--- Chapter_01/seq_and_collections.py
+++ Chapter_01/seq_and_collections.py
@@ -2,6 +2,21 @@ a = reversed('hello')
 print(a)
 print(*a)
 
-b = (1, 2, 3)   # tuple
+b = (1, 2, 3, 2)   # tuple
+# b[2] = 4 - only read
 print(b)
 print(*b)
+
+c = [1, 2, 3, 3]   # list
+c[3] = 4
+print(c)
+print(*c)
+
+d = {1, 2, 3, 3}   # set - unique values
+print(d)
+print(*d)
+
+e = {1: 'one', 2: 'two'}    # dist
+print(e)
+print(*e)
+print(e[1])
